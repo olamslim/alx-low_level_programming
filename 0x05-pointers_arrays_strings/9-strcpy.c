88bf8e8f326143d123deba4ee62f4e206db93895
olamslim <debeabdul22@gmail.com>
2022-10-25 20:47:07 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src
+ * *_strcpy - copies the string pointed to by src
  * @dest: destination
  * @src: source
  * Return: string
